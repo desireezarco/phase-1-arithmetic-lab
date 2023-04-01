52aab612dbcefad11231d2cc9839c8c5a0e91284
Desiree Zarco <desireeannzarco7@gmail.com>
2023-04-01 14:59:34 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
-// Write your code here
+{let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+}
+
+
+let random = 1;
+Math.random();
+
+
+
+let num3 = 8;
+let num4 = 2;
+let mod = (num3 / num4);
+
+
+let max = (2, 4, 6, 8, 10, 20);
+Math.max (20);
